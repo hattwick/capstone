a37a2bb34dd742b807c2de86ceef2d1debc2fead
hattwick <phil@hattwick.com>
2016-04-10 11:38:25 -0400

    homework 1 bundle

--- spider.py
+++ spider.py
@@ -2,6 +2,7 @@
 # This program is part of a sequence of 4 to
 # (1) Spider a URL for some # of links into a SQLITE database, (2) Rank each page, (3) dump the database to a file
 # and finally output to JSON for a d3.js visualization
+# Execution order: spider.py, sprank.py, spdump.py, spjson.py
 
 import sqlite3
 import urllib
