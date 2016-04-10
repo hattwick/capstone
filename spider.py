671b2ef65e9855428179cfb5d3268e8c1f832e7a
hattwick <phil@hattwick.com>
2016-04-10 11:36:22 -0400

    homework 1 bundle

--- spider.py
+++ spider.py
@@ -1,4 +1,7 @@
 # Coursera - Python Capstone HW1
+# This program is part of a sequence of 4 to
+# (1) Spider a URL for some # of links into a SQLITE database, (2) Rank each page, (3) dump the database to a file
+# and finally output to JSON for a d3.js visualization
 
 import sqlite3
 import urllib
