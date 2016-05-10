648726fa0d0b1ba061a29b0d42057944cf7b11a6
hattwick <phil@hattwick.com>
2016-05-09 22:42:21 -0400

    indentation fix

--- scrape4.py
+++ scrape4.py
@@ -7,5 +7,5 @@ from bs4 import BeautifulSoup
 html = urlopen("http://dowjones.com")
 bsObj = BeautifulSoup(html)
 for link in bsObj.findAll("a"):
-if 'href' in link.attrs:
-print(link.attrs['href'])
+    if 'href' in link.attrs:
+        print(link.attrs['href'])
