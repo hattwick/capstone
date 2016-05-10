02b9b243ba7bc5d8c8f0ac6015e547dd41e59184
hattwick <phil@hattwick.com>
2016-05-09 22:41:01 -0400

    scrape

---
+++ scrape4.py
@@ -0,0 +1,11 @@
+# From Web Scraping with Python with modifications
+__author__ = 'phil'
+
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+html = urlopen("http://dowjones.com")
+bsObj = BeautifulSoup(html)
+for link in bsObj.findAll("a"):
+if 'href' in link.attrs:
+print(link.attrs['href'])
